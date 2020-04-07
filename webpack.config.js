cdafe3641c5b346be55ede063dcf41efa7a96313
Philippe Bourdages <philippe.bourdages@gmail.com>
2020-04-06 21:53:29 -0400

    Remove css support

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,8 @@
 const path = require("path");
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
-const htmlPlugin = new HtmlWebPackPlugin({
-    template: "src/index.html",
-    hash: true,
-});
-
-const miniCSSPlugin = new MiniCssExtractPlugin({
-    filename: "[name].css",
-    chunkFilename: "[id].css",
-});
 
 module.exports = {
     entry: path.resolve(__dirname, "./src/index.js"),
@@ -40,15 +29,6 @@ module.exports = {
                 },
             },
             {
-                test: /\.scss$/,
-                use: [
-                    process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader",
-                ],
-            },
-            {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: ["file-loader"],
             },
@@ -59,8 +39,10 @@ module.exports = {
         ],
     },
     plugins: [
-        htmlPlugin,
-        miniCSSPlugin,
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            hash: true,
+        }),
         // new BundleAnalyzerPlugin()
     ],
 };
